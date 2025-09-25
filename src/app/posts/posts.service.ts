df390ba06579c9d6d7d80a46c98e8c6a40549e61
balazs.brigovacz <balazs_brigovacz@epam.com>
2025-09-25 15:23:05 +0200

    post

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -25,7 +25,10 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = {id: 'null', title: title, content: content};
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+      .subscribe(responseData => {
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 }
