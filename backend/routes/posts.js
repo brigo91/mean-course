9842e2cd427547b731a3c07adba18e1fb81d6748
balazs.brigovacz <balazs_brigovacz@epam.com>
2025-10-01 18:16:07 +0200

    90. Connecting the Angular Paginator to the Backend

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -71,7 +71,13 @@ router.put(
 );
 
 router.get('', (req, res, next) => {
-    Post.find().then(documents => {
+    const pageSize = +req.query.pagesize;
+    const currentPage = +req.query.page;
+    const postQuery = Post.find();
+    if (pageSize && currentPage) {
+      postQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
+    }
+    postQuery.then(documents => {
         res.status(200).json({
             message: 'Post fetched successully',
             posts: documents
